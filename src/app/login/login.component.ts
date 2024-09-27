80a4c1b8c23424ab409b6ff4fb0cf0a130fb8d25
Difroy <unifroy@gmail.com>
2024-09-27 12:47:49 +0200

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Admin } from '../../model/Admin';
+import { FormsModule } from '@angular/forms';
 
 @Component
 ({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })

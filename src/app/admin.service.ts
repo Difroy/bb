4363a5355089a9edb85e64437e6764a050eadffd
Difroy <unifroy@gmail.com>
2024-09-27 12:59:44 +0200

    sa

---
+++ src/app/admin.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Admin } from '../model/Admin';
+import { catchError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService
+{
+  admin?:Admin;
+  APIURL:string = 'http://localhost:8080/bb/api/admin/login'
+  constructor(private http:HttpClient)
+  {
+
+
+
+  }
+login(admin:Admin):void
+
+{
+
+this
+.http
+.post<any>(this.APIURL, admin)
+.subscribe(() =>this.admin = admin);
+
+}
+
+}

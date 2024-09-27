4f9db7def2b716a4b46739ac7ee49719012d26cf
Difroy <unifroy@gmail.com>
2024-09-27 12:47:30 +0200

    uipda

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Admin } from '../../model/Admin';
 
-@Component({
+@Component
+({
   selector: 'app-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent
+{
+
+
+admin:Admin = { email:'', password:''}
+
 
 }
